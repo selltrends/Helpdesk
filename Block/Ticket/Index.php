27c4a80d6ecf4bac6845df9ba92ddb8ba1ec18fb
Alex Wang <selltrends@gmail.com>
2016-10-25 15:00:56 +0800

    amend controller file dir & Creating blocks and templates. Need to amend route error!

---
+++ Block/Ticket/Index.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Atopt\Helpdesk\Block\Ticket;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime
+     */
+    protected $dateTime;
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $customerSession;
+    /**
+     * @var \Atopt\Helpdesk\Model\TicketFactory
+     */
+    protected $ticketFactory;
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Stdlib\DateTime $dateTime,
+        \Magento\Customer\Model\Session $customerSession,
+        \Atopt\Helpdesk\Model\TicketFactory $ticketFactory,
+        array $data = []
+    )
+    {
+        $this->dateTime = $dateTime;
+        $this->customerSession = $customerSession;
+        $this->ticketFactory = $ticketFactory;
+        parent::__construct($context, $data);
+    }
+    /**
+     * @return \Atopt\Helpdesk\Model\ResourceModel\Ticket\Collection
+     */
+    public function getTickets()
+    {
+        return $this->ticketFactory
+            ->create()
+            ->getCollection()
+            ->addFieldToFilter('customer_id', $this->customerSession->getCustomerId());
+    }
+    public function getSeverities()
+    {
+        return \Atopt\Helpdesk\Model\Ticket::getSeveritiesOptionArray();
+    }
+}
