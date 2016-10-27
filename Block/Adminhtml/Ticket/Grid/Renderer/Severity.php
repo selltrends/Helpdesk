bc3263d3631a047f71bffad401371c966b0e8c34
Alex Wang <selltrends@gmail.com>
2016-10-27 10:46:42 +0800

    Creating a grid column renderer

---
+++ Block/Adminhtml/Ticket/Grid/Renderer/Severity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Atopt\Helpdesk\Block\Adminhtml\Ticket\Grid\Renderer;
+
+class Severity extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
+{
+    protected $ticketFactory;
+    public function __construct(
+        \Magento\Backend\Block\Context $context,
+        \Atopt\Helpdesk\Model\TicketFactory $ticketFactory,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->ticketFactory = $ticketFactory;
+    }
+    public function render(\Magento\Framework\DataObject $row)
+    {
+        $ticket = $this->ticketFactory->create()->load($row->getId());
+        if ($ticket && $ticket->getId()) {
+            return $ticket->getSeverityAsLabel();
+        }
+        return '';
+    }
+}
